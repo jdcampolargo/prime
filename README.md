ec261a9c2e1904812ea2d04cadbd2a5702550962
jdcampolargo <jdcampolargo@hotmail.com>
2023-03-29 19:37:48 -0500

    Pushing Updates

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# prime
+# Prime Numbers
+
+This is a repo where I'll be testing different prime number algorithms.
--- prime.py
+++ prime.py
@@ -1,3 +1,18 @@
-print("py")
+## Write code to record the difference between primes and also record the longest difference you can find between prime numbers. 
 
+import math
 
+def is_prime(n):
+    if n < 2:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    max = math.floor(math.sqrt(n))
+    i = 3
+    while i <= max:
+        if n % i == 0:
+            return False
+        i += 2
+    return True
